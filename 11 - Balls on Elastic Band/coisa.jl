d422245232de247af9290cfb2cba29a38eafc518
J. Marchi <jmarchi.oliveira@gmail.com>
2022-05-19 23:12:39 -0300

    Still diverges, check phi dots

--- 11 - Balls on Elastic Band/coisa.jl
+++ 11 - Balls on Elastic Band/coisa.jl
@@ -2,7 +2,7 @@ using DifferentialEquations, StaticArrays, LinearAlgebra, BenchmarkTools, Plots
 
 function Fel(l, phi)
     k = 1
-    return k * l
+    return k * (l - 3)
 end
 
 function Tel(l, phi)
@@ -48,8 +48,8 @@ function eq_motion_dif(u, p, t)
 
     d = sqrt(l ^ 2 + ΔR ^ 2)
     fel = Fel(d - ΣR, ϕ₁ + ϕ₂)
-    adjusted_fel = -fel * l / d
-    tel = Tel(d - ΣR, ϕ₁ + ϕ₂)
+    adjusted_fel = fel * l / d
+    tel = -Tel(d - ΣR, ϕ₁ + ϕ₂)
 
     N₁ = m₁ * g - fel * ΔR / d
     ∂γ₁ = μ_m1 * θ_dot - R₁ * ϕ₁_dot / d
@@ -57,7 +57,7 @@ function eq_motion_dif(u, p, t)
     if vd₁ != 0
         Fat1_mod = μ₁ * N₁ / vd₁
         Fat1_x = -Fat1_mod * (μ_m1 * l_dot * cos(θ) - ∂γ₁ * l * sin(θ))
-        Fat1_y = -Fat1_mod * (μ_m1 * l_dot * sin(θ) - ∂γ₁ * l * cos(θ))
+        Fat1_y = -Fat1_mod * (μ_m1 * l_dot * sin(θ) + ∂γ₁ * l * cos(θ))
     else
         Fat1_x = Fat1_y = 0
     end
@@ -67,22 +67,21 @@ function eq_motion_dif(u, p, t)
     vd₂ = sqrt((μ_m2 * l_dot) ^ 2 + (∂γ₂ * l) ^ 2)
     if vd₂ != 0
         Fat2_mod = μ₂ * N₂ / vd₂
-        Fat2_x = -Fat2_mod * (μ_m2 * l_dot * cos(θ) + ∂γ₂ * l * sin(θ))
-        Fat2_y = -Fat2_mod * (μ_m2 * l_dot * sin(θ) + ∂γ₂ * l * cos(θ))
+        Fat2_x = -Fat2_mod * (-μ_m2 * l_dot * cos(θ) + ∂γ₂ * l * sin(θ))
+        Fat2_y = -Fat2_mod * (-μ_m2 * l_dot * sin(θ) - ∂γ₂ * l * cos(θ))
     else
         Fat2_x = Fat2_y = 0
     end
 
     drx_dot = (Fat1_x + Fat2_x) / M; dry_dot = (Fat1_y + Fat2_y) / M
-    dθ_dot = ((adjusted_fel * sin(θ) + Fat1_y - m₁ * dry_dot) * cos(θ) - (adjusted_fel * cos(θ) + Fat1_x - m₁ * drx_dot) * sin(θ) - 2 * l_dot * θ_dot) / l
-    dl_dot = ((adjusted_fel * cos(θ) + Fat1_x - m₁ * drx_dot) * cos(θ) + (adjusted_fel * sin(θ) + Fat1_y - m₁ * dry_dot) * sin(θ) + l * dθ ^ 2) / μ
+    dθ_dot = (((m₁ * drx_dot - Fat1_x) * sin(θ) + (Fat1_y - m₁ * dry_dot) * cos(θ)) / μ- 2 * l_dot * θ_dot) / l
+    dl_dot = ((Fat1_x - m₁ * drx_dot) * cos(θ) + (Fat1_y - m₁ * dry_dot) * sin(θ) - adjusted_fel) / μ + l * θ_dot ^ 2
     dϕ₁_dot = ϕ₁_dot * (l_dot * (-(ΔR^2 * l_dot)/ (d^2 * l)) + θ_dot * tan(θ)) + (tel - fel * R₁ * tan(θ) - (m₁ * d * R₁ * drx_dot / cos(θ) - μ * d ^ 2 * (
               (dl_dot - l * θ_dot ^ 2) * (R₁ * (tan(θ) / m₁ + 1 / m₂) + R₂ / m₁ * (tan(θ) - 1)) -  (l * dθ_dot + 2 * l_dot * θ_dot) * (R₁ * (tan(θ) / m₂ - 1 / m₁) +
               R₂ / m₁ * (tan(θ) + 1)))) / l) / I₁
     dϕ₂_dot = I₁ / I₂ * dϕ₁_dot - μ * ΔR / I₂ * d * dθ_dot + (ϕ₂_dot - I₁ * ϕ₁_dot / I₂) * l * l_dot / d ^ 2 + d / (I₂ * ΔR) * (2 * μ * l * l_dot * θ_dot + (μ *
               d ^ 2 + I₁ + I₂) * dθ_dot + Fat1_x * (ry + μ_m1 * l * cos(θ)) - Fat1_y * (rx - μ_m1 * l * cos(θ)) + Fat2_x * (ry + μ_m2 * l * sin(θ)) - Fat2_y *
               (rx + μ_m2 * l * cos(θ)))
-
     SA[drx, dry, dθ, dl, dϕ₁, dϕ₂, drx_dot, dry_dot, dθ_dot, dl_dot, dϕ₁_dot, dϕ₂_dot]
 end
 
@@ -97,7 +96,7 @@ function build_p(m::Float64, R::Float64, μ::Float64)
 end
 
 p = build_p(1., 2., 1., 2., 1., 1.)
-u0 = [0., 0., 0., 1., 100., 100., 0, 0, 0, 0, 0, 0]
+u0 = [0., 0., 0., 4., 100., 100., 0, 0, 0, 0, 0, 0]
 tspan = (0., 100.)
 ode_problem = ODEProblem{Any}
 if length(p) == 15
@@ -107,3 +106,4 @@ elseif length(p) == 5
 end
 
 sol = solve(ode_problem)
+plot(sol,tspan=(0.,0.5))
