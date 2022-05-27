17a81e2675329a5fb8b574e5bcfa9a380c8a2790
J. Marchi <jmarchi.oliveira@gmail.com>
2022-05-27 05:13:36 -0300

    quase

--- 11 - Balls on Elastic Band/coisa.jl
+++ 11 - Balls on Elastic Band/coisa.jl
@@ -1,23 +1,21 @@
-using DifferentialEquations, StaticArrays, LinearAlgebra, BenchmarkTools, Plots, DataFrames, CSV
+using DifferentialEquations, StaticArrays, LinearAlgebra, DataFrames, CSV, Interpolations
 
-function Fel(l, phi)
-    k = 7.5
+function Fel(l, phi, k)
     return k * (l - 0.06)
 end
 
-function Tel(l, phi)
-    K = 1
+function Tel(l, phi, K)
     return K * phi
 end
 
 function eq_motion_same(u, p, t)
     θ, l, ϕ, θ_dot, l_dot, ϕ_dot = u
-    m, R, I, μ, g = p
+    m, R, I, μ, k, K, g = p
 
     dθ = θ_dot; dl = l_dot; dϕ = ϕ_dot
 
-    fel = Fel(l - 2 * R, 2 * ϕ)
-    tel = -Tel(l - 2 * R, 2 * ϕ)
+    fel = Fel(l - 2 * R, 2 * ϕ, k)
+    tel = -Tel(l - 2 * R, 2 * ϕ, K)
 
     N = m * g
     ∂γ = θ_dot / 2 - R * ϕ_dot / l
@@ -39,16 +37,16 @@ end
 
 function eq_motion_dif(u, p, t)
     rx, ry, θ, l, ϕ₁, ϕ₂, rx_dot, ry_dot, θ_dot, l_dot, ϕ₁_dot, ϕ₂_dot = u
-    m₁, m₂, μ, μ_m1, μ_m2, M, R₁, R₂, ΔR, ΣR, I₁, I₂, μ₁, μ₂, g = p
+    m₁, m₂, μ, μ_m1, μ_m2, M, R₁, R₂, ΔR, ΣR, I₁, I₂, μ₁, μ₂, k, K, g = p
 
     drx = rx_dot; dry = ry_dot
     dθ = θ_dot; dl = l_dot
     dϕ₁ = ϕ₁_dot; dϕ₂ = ϕ₂_dot
 
     d = sqrt(l ^ 2 + ΔR ^ 2)
-    fel = Fel(d - ΣR, ϕ₁ + ϕ₂)
+    fel = Fel(d - ΣR, ϕ₁ + ϕ₂, k)
     adjusted_fel = fel * l / d
-    tel = -Tel(d - ΣR, ϕ₁ + ϕ₂)
+    tel = -Tel(d - ΣR, ϕ₁ + ϕ₂, K)
 
     N₁ = m₁ * g - fel * ΔR / d
     ∂γ₁ = μ_m1 * θ_dot - R₁ * ϕ₁_dot / d
@@ -82,54 +80,107 @@ function eq_motion_dif(u, p, t)
     dϕ₂_dot = I₁ / I₂ * dϕ₁_dot - μ * ΔR / I₂ * d * dθ_dot + (ϕ₂_dot - I₁ * ϕ₁_dot / I₂) * l * l_dot / d ^ 2 + d / (I₂ * ΔR) * (2 * μ * l * l_dot * θ_dot + (μ *
               d ^ 2 + I₁ + I₂) * dθ_dot + Fat1_x * (ry + μ_m1 * l * cos(θ)) - Fat1_y * (rx - μ_m1 * l * cos(θ)) + Fat2_x * (ry + μ_m2 * l * sin(θ)) - Fat2_y *
               (rx + μ_m2 * l * cos(θ)))
+
     SA[drx, dry, dθ, dl, dϕ₁, dϕ₂, drx_dot, dry_dot, dθ_dot, dl_dot, dϕ₁_dot, dϕ₂_dot]
 end
 
-function build_p(m1::Float64, m2::Float64, R1::Float64, R2::Float64, μ₁::Float64, μ₂::Float64)
+function build_p(m1::Real, m2::Real, R1::Real, R2::Real, μ₁::Real, μ₂::Real, k::Real, K::Real)
     return [m1, m2, m1 * m2 / (m1 + m2), m2 / (m1 + m2), m1 / (m1 + m2), m1 + m2,
            R1, R2, R2 - R1, R1 + R2, (2 / 5) * m1 * R1 ^ 2, (2 / 5) * m2 * R2 ^ 2,
-           μ₁, μ₂, 9.8]
+           μ₁, μ₂, k, K, 9.8]
 end
 
-function build_p(m::Float64, R::Float64, μ::Float64)
-    return [m, R, (2 / 5) * m * R ^ 2, μ, 9.8]
+function build_p(m::Real, R::Real, μ::Real, k::Real, K::Real)
+    return [m, R, (2 / 5) * m * R ^ 2, μ, k, K, 9.8]
 end
 
-p = build_p(0.06, 0.02549, 0.3)
-tspan = (0., 100.)
-ode_problem = ODEProblem{Any}
-if length(p) == 15
-    u0 = [0., 0., 0., 4., 100., 100., 0, 0, 0, 0, 0, 0]
-    m₁, m₂, μ, μ_m1, μ_m2, M, R₁, R₂, ΔR, ΣR, I₁, I₂, μ₁, μ₂, g = p
-    ode_problem = ODEProblem(eq_motion_dif, u0, tspan, p)
-elseif length(p) == 5
-    u0 = [0., .10, 50., 0., 0., 0.]
-    m, R, I, μ, g = p
-    ode_problem = ODEProblem(eq_motion_same, u0, tspan, p)
+function build_p(p::Tuple)
+    if length(p) == 5
+        return build_p(p[1], p[2], p[3], p[4], p[5])
+    elseif length(p) == 8
+        return build_p(p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8])
+    end
 end
 
-sol = solve(ode_problem)
-data = DataFrame(sol)
-try
-    rename!(data, [:timestamp, :value1, :value2, :value3, :value4, :value5, :value6,
-                   :value7, :value8, :value9, :value10, :value11, :value12] .=> [:t,
-                   :rx, :ry, :θ, :l, :ϕ₁, :ϕ₂, :rx_dot, :ry_dot, :θ_dot, :l_dot, :ϕ₁_dot,
-                   :ϕ₂_dot])
-    data.∂γ₁ = μ_m1 * data.θ_dot - R₁ * data.ϕ₁_dot ./ sqrt.(data.l .^ 2 .+ ΔR ^2)
-    data.∂γ₂ = μ_m2 * data.θ_dot - R₂ * data.ϕ₂_dot ./ sqrt.(data.l .^ 2 .+ ΔR ^2)
-    data.rx1star = μ_m1 * data.l .* cos.(data.θ)
-    data.rx1 = data.rx + μ_m1 * data.rx1star
-    data.ry1star = μ_m1 * data.l .* sin.(data.θ)
-    data.ry1 = data.ry + data.ry1star
-    data.rx2star = -μ_m2 * data.l .* cos.(data.θ)
-    data.rx2 = data.rx + data.rx2star
-    data.ry2star = -μ_m2 * data.l .* sin.(data.θ)
-    data.ry2 = data.ry + data.ry2star
-catch
-    rename!(data, [:timestamp, :value1, :value2, :value3, :value4, :value5, :value6]
-                   .=> [:t, :θ, :l, :ϕ, :θ_dot, :l_dot, :ϕ_dot])
-    data.∂γ = data.θ_dot ./ 2 - R * data.ϕ_dot ./ data.l
-    data.rx1 = -data.l .* cos.(data.θ)
-    data.ry1 = -data.l .* sin.(data.θ)
-    data.rx2 = -data.rx1; data.ry2 = -data.ry1
+function build_p_special(p::Tuple)
+    return [p[1], p[2], p[3], p[4], p[5], p[6], p[7]]
+end
+
+function terminate_affect!(integrator)
+    terminate!(integrator)
+end
+
+function terminate_same(u, t, integrator)
+    (u[4] > 0.1 && t > 0.1)
+end
+
+function terminate_diff(u, t, integrator)
+    (u[9] < -0.1 && t > 0.1)
+end
+
+terminate_cb_same = DiscreteCallback(terminate_same, terminate_affect!)
+terminate_cb_diff= DiscreteCallback(terminate_diff, terminate_affect!)
+
+function solution(p::Vector, u0::Vector)
+    tspan = (0., 100.)
+    ode_problem = ODEProblem{Any}
+    if length(p) == 17
+        ode_problem = ODEProblem(eq_motion_dif, u0, tspan, p, callback = terminate_cb_diff)
+    elseif length(p) == 7
+        ode_problem = ODEProblem(eq_motion_same, u0, tspan, p, callback = terminate_cb_same)
+    end
+    sol = solve(ode_problem)
+    data = DataFrame(sol)
+    return clean_data(data, p)
+end
+
+function full_solution(p::Vector, u0::Vector)
+    tspan = (0., 100.)
+    ode_problem = ODEProblem{Any}
+    if length(p) == 17
+        ode_problem = ODEProblem(eq_motion_dif, u0, tspan, p)
+    elseif length(p) == 7
+        ode_problem = ODEProblem(eq_motion_same, u0, tspan, p)
+    end
+    sol = solve(ode_problem, saveat=0.:0.01:100.)
+    data = DataFrame(sol)
+    return clean_data(data, p)
+end
+
+function clean_data(data::DataFrame, p::Vector)
+    try
+        _, _, _, μ_m1, μ_m2, _, R₁, R₂, ΔR, _, _, _, _, _, _ = p
+        rename!(data, [:timestamp, :value1, :value2, :value3, :value4, :value5, :value6,
+                       :value7, :value8, :value9, :value10, :value11, :value12] .=> [:t,
+                       :rx, :ry, :θ, :l, :ϕ₁, :ϕ₂, :rx_dot, :ry_dot, :θ_dot, :l_dot, :ϕ₁_dot,
+                       :ϕ₂_dot])
+        data.∂γ₁ = μ_m1 * data.θ_dot - R₁ * data.ϕ₁_dot ./ sqrt.(data.l .^ 2 .+ ΔR ^2)
+        data.∂γ₂ = μ_m2 * data.θ_dot - R₂ * data.ϕ₂_dot ./ sqrt.(data.l .^ 2 .+ ΔR ^2)
+        data.rx1star = μ_m1 * data.l .* cos.(data.θ)
+        data.rx1 = data.rx + μ_m1 * data.rx1star
+        data.ry1star = μ_m1 * data.l .* sin.(data.θ)
+        data.ry1 = data.ry + data.ry1star
+        data.rx2star = -μ_m2 * data.l .* cos.(data.θ)
+        data.rx2 = data.rx + data.rx2star
+        data.ry2star = -μ_m2 * data.l .* sin.(data.θ)
+        data.ry2 = data.ry + data.ry2star
+    catch
+        R = p[2]
+        rename!(data, [:timestamp, :value1, :value2, :value3, :value4, :value5, :value6]
+                    .=> [:t, :θ, :l, :ϕ, :θ_dot, :l_dot, :ϕ_dot])
+        data.∂γ = data.θ_dot ./ 2 - R * data.ϕ_dot ./ data.l
+        data.rx1 = -data.l .* cos.(data.θ)
+        data.ry1 = -data.l .* sin.(data.θ)
+        data.rx2 = -data.rx1; data.ry2 = -data.ry1
+    end
+    return data
+end
+
+function inv_time(data::DataFrame)
+    for i in 100:length(data.t)
+        t = -(data.t[i] * data.θ_dot[i - 1] - data.t[i - 1] * data.θ_dot[i]) / (data.θ_dot[i] - data.θ_dot[i - 1])
+        if t > data.t[i - 1] && t < data.t[i]
+            return t
+        end
+    end
 end
---
+++ 11 - Balls on Elastic Band/plots.jl
@@ -0,0 +1,70 @@
+using Plots, XLSX
+include("coisa.jl")
+
+p_same = build_p(0.06, 0.02549, 0.3, 7.5, 0.00001)
+u0_same = [0., .1, 50., 0., 0., 0.]
+
+μ_range = 0.1:0.01:0.5
+I_range = 0.000001:0.000001:0.00004
+k_range = 5.:100.
+K_range = 0.00001:0.000001:0.0001
+
+ϕ₀_range = 20.:2.:200.
+
+p_μ = build_p.([(0.06, 0.02549, μ, 7.5, 0.00001) for μ in μ_range])
+p_I = build_p_special.([(0.06, 0.02549, I, 0.3, 7.5, 0.00001, 9.8) for I in I_range])
+p_k = build_p.([(0.06, 0.02549, 0.3, k, 0.00001) for k in k_range])
+p_K = build_p.([(0.06, 0.02549, 0.3, 7.5, K) for K in K_range])
+
+u0_ϕ₀ = [[0., .1, ϕ₀, 0., 0., 0.] for ϕ₀ in ϕ₀_range]
+
+data_μ = solution.(p_μ, [u0_same for _ in 1:length(p_μ)])
+times_μ = inv_time.(data_μ)
+df_μ = DataFrame("μ" => μ_range, "t" => times_μ)
+
+data_I = solution.(p_I, [u0_same for _ in 1:length(p_I)])
+times_I = inv_time.(data_I)
+df_I = DataFrame("I" => I_range, "t" => times_I)
+
+data_k = solution.(p_k, [u0_same for _ in 1:length(p_k)])
+times_k = inv_time.(data_k)
+df_k = DataFrame("k" => k_range, "t" => times_k)
+
+data_K = solution.(p_K, [u0_same for _ in 1:length(p_K)])
+times_K = inv_time.(data_K)
+df_K = DataFrame("K" => K_range, "t" => times_K)
+
+data_ϕ₀ = solution.([p_same for _ in 1:length(u0_ϕ₀)], u0_ϕ₀)
+times_ϕ₀ = inv_time.(data_ϕ₀)
+df_ϕ₀ = DataFrame("ϕ₀" => ϕ₀_range, "t" => times_ϕ₀)
+
+CSV.write("dados/txmu.csv", df_μ)
+CSV.write("dados/txI.csv", df_I)
+CSV.write("dados/txk.csv", df_k)
+CSV.write("dados/txkappa.csv", df_K)
+CSV.write("dados/txphi0.csv", df_ϕ₀)
+
+data = full_solution(build_p(0.6, 0.02549, 0.3, 7.5, 0.000005),[0., .1, 50., 0., 0., 0.])
+
+# plt1 = plot(μ_range, times_μ, legend = false)
+# plt2 = plot(I_range, times_I, legend = false)
+# plt3 = plot(k_range, times_k, legend = false)
+# plt4 = plot(K_range, times_K, legend = false)
+# plt5 = plot(ϕ₀_range, times_ϕ₀, legend = false)
+
+# u0_diff = [0., 0., 0., .1, 50., 50., 0., 0., 0., 0., 0., 0.]
+
+# m₁ = 0.0601; m₂ = m₁ / 2; m₂_range = 0.001:0.001:0.1
+# R₁ = 0.02549; R₂ = R₁ / 2; R₂_range = 0.005:0.001:0.1
+
+# p_m = build_p.([(m₁, i, R₁, R₂, 0.3, 0.3, 7.5, 0.00001) for i in m₂_range])
+# p_R = build_p.([(m₁, m₂, R₁, i, 0.3, 0.3, 7.5, 0.00001) for i in R₂_range])
+
+# data_m = solution.(p_m, [u0_diff for _ in 1:length(p_m)])
+# times_m = inv_time.(data_m)
+
+# data_R = solution.(p_R, [u0_diff for _ in 1:length(p_R)])
+# times_R = inv_time.(data_R)
+
+# plt6 = plot(m₂_range / m₁, times_m)
+# plt7 = plot(R₂_range / R₁, times_R)
